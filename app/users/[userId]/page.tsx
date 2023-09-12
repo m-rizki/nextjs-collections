ecb338b6a73c00f6dcad1e6607da31dd87d0ddee
Muhamad Rizki <37399436+m-rizki@users.noreply.github.com>
2023-09-12 23:12:56 +0700

    Add Suspense and dynamic metadata

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,6 +1,10 @@
+import { Suspense } from "react";
+import type { Metadata } from "next";
+
 import getUser from "@/lib/get-user";
 import getUserPosts from "@/lib/get-user-posts";
 import { Post, User } from "@/types";
+import UserPosts from "./components/user-posts";
 
 type Params = {
   params: {
@@ -8,21 +12,36 @@ type Params = {
   };
 };
 
+// Dynamic Metadata
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  // Duplicate fetch
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  };
+}
+
 const UserPage = async ({ params: { userId } }: Params) => {
   const userData: Promise<User> = getUser(userId);
   const userPostData: Promise<Post[]> = getUserPosts(userId);
 
   // requesting data in parallel
-  const [user, userPosts] = await Promise.all([userData, userPostData]);
+  // const [user, userPosts] = await Promise.all([userData, userPostData]);
 
+  const user = await userData;
   return (
     <>
-      <h1>User Page</h1>
-      <br />
-      {/* <h2>{user.name}</h2> */}
+      <h1 className="text-3xl font-bold">{user.name}</h1>
       <br />
       {/* <h2>{userPosts[1].title}</h2> */}
-      {/* <UserPosts posts={userPosts} /> */}
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserPosts promise={userPostData} />
+      </Suspense>
     </>
   );
 };
