02beecffd8dd3e5b0b48abe3fed28f0c7c8fdcdc
Muhamad Rizki <37399436+m-rizki@users.noreply.github.com>
2023-09-17 12:34:46 +0700

    Add another method for dynamic routes

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,10 +2,18 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export async function GET(request: Request) {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// export async function GET(request: Request) {
+
+export async function GET(request: Request, { params: { id } }: Props) {
   // https://jsonplaceholder.typicode.com/todos/1
   // id = 1
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+  // const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
